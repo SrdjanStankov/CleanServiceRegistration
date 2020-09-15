7b2ba23b3c41c77176c875a35c206704f48af6c9
Srdjan Stankov <stankovsr@gmail.com>
2020-09-15 18:22:37 +0200

    Added Clean Service Registration

---
+++ CleanServiceRegistration/Installers/ControllersInstaller.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanServiceRegistration.Installers
+{
+    public class ControllersInstaller : IInstaller
+    {
+        public void InstallServices(IConfiguration configuration, IServiceCollection services) => services.AddControllers();
+    }
+}
---
+++ CleanServiceRegistration/Installers/InstallerExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanServiceRegistration.Installers
+{
+    public static class InstallerExtensions
+    {
+        public static void AddServicesInAssembly(this IServiceCollection services, IConfiguration configuration)
+        {
+            var installers = typeof(Startup).Assembly.ExportedTypes
+                .Where(type => typeof(IInstaller).IsAssignableFrom(type) && !type.IsInterface && !type.IsAbstract)
+                .Select(Activator.CreateInstance)
+                .Cast<IInstaller>()
+                .ToList();
+            installers.ForEach(i => i.InstallServices(configuration, services));
+        }
+    }
+}
